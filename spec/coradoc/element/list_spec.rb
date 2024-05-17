98133fd5a79e4749989515c5eb2f3150f1fed681
xyz65535 <xyz65535@gazeta.pl>
2024-05-17 17:34:53 +0200

    inline formatting, attribute list validation, definition lists

--- spec/coradoc/element/list_spec.rb
+++ spec/coradoc/element/list_spec.rb
@@ -9,5 +9,23 @@ RSpec.describe Coradoc::Element::List do
 
       expect(list.items).to eq(items)
     end
+    it "handles list continuations" do
+      items2 = Coradoc::Element::ListItem.new(["Item 2a", "Item 2b", "Item 2c"])
+      item1 = Coradoc::Element::ListItem.new("Item 1")
+      items = [item1, items2]
+
+      list = Coradoc::Element::List::Unordered.new(items)
+
+      expect(list.to_adoc).to eq("\n\n* Item 1\n* Item 2a\n+\nItem 2b\n+\nItem 2c\n")
+    end
+    it "handles definition list" do
+      # items = Coradoc::Element::ListItem.new(["Item1", "Item 2b", "Item 2c"])
+      item = Coradoc::Element::ListItemDefinition.new("Coffee","Black hot drink")
+      items = [item]
+
+      list = Coradoc::Element::List::Definition.new(items)
+
+      expect(list.to_adoc).to eq("\nCoffee:: Black hot drink\n")
+    end
   end
 end
