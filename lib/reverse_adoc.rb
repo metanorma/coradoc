a0fe5450a3bb52df321e522c7d9edfdfdc0d4451
hmdne <54514036+hmdne@users.noreply.github.com>
2024-05-21 06:53:09 +0200

    Merger: Add a thin wrapper to support not-merge-aware software

---
+++ lib/reverse_adoc.rb
@@ -0,0 +1,20 @@
+warn <<~END
+  Deprecated: reverse_adoc has been merged into coradoc gem.
+  | Please update your references from:
+  |   require 'reverse_adoc'
+  | To:
+  |   require 'coradoc/reverse_adoc'
+  |
+  | You are referencing an old require here:
+  |   #{caller.join("\n|   ")}
+  |
+  | You should also replace 'coradoc' with 'reverse_adoc' in your gem dependencies.
+  | reverse_adoc 2.0.0 will be kept with 'coradoc' as the only dependency.
+  |
+  | Please also ensure that you replace all references to ReverseAdoc in your code
+  | with Coradoc::ReverseAdoc.
+END
+
+require "coradoc/reverse_adoc"
+
+ReverseAdoc = Coradoc::ReverseAdoc
