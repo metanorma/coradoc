4323b1665a582c6c10d850682a26c650df4c71f4
hmdne <54514036+hmdne@users.noreply.github.com>
2024-05-23 04:33:57 +0200

    Further code cleanup

--- lib/coradoc/element/inline/bold.rb
+++ lib/coradoc/element/inline/bold.rb
@@ -4,15 +4,18 @@ module Coradoc
       class Bold
         attr_accessor :content, :unconstrained
 
-        def initialize(content, unconstrained = true)
+        def initialize(content, unconstrained: true)
           @content = content
           @unconstrained = unconstrained
         end
 
         def to_adoc
           content = Coradoc::Generator.gen_adoc(@content)
-          doubled = @unconstrained ? "*" : ""
-          "#{doubled}*#{content}*#{doubled}"
+          if @unconstrained
+            "**#{content}**"
+          else
+            "*#{content}*"
+          end
         end
       end
     end
