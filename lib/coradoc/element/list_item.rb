1fddfa10b6d34273598a8bf4bcfeed45f5981e8b
hmdne <54514036+hmdne@users.noreply.github.com>
2024-12-10 20:55:07 +0100

    Coradoc::Input::HTML: Fix complex list items
    This commit fixes handling of complex list items.
    
    Before Coradoc refactor, reverse_adoc used to treat all
    children of list elements as inline elements (if I recall correctly)
    
    After Coradoc refactor, we treat all children of list
    elements as non-online elements (necessitating line break).
    
    This broke certain assumptions we have while dealing with
    BIPM incoming documents. In particular, whenever there was
    a subscript involved, we get unexpected line breaks.
    
    This PR aims to correctly handle all inline and non-inline
    list children, according to AsciiDoc specification[1].
    
    Since this is quite a large refactor of a crucial part of
    documents, this may break a couple of assumptions, so
    I would really appreciate some testing before merging that.
    
    [1] https://docs.asciidoctor.org/asciidoc/latest/lists/continuation/

--- lib/coradoc/element/list_item.rb
+++ lib/coradoc/element/list_item.rb
@@ -15,26 +15,77 @@ module Coradoc
         @line_break = options.fetch(:line_break, "\n")
       end
 
+      def inline?(elem)
+        case elem
+        when Inline::HardLineBreak
+          :hardbreak
+        when ->(i){ i.class.name.to_s.include? "::Inline::" }
+          true
+        when String, TextElement, Image::InlineImage
+          true
+        else
+          false
+        end
+      end
+
       def to_adoc
         anchor = @anchor.nil? ? "" : " #{@anchor.to_adoc.to_s} "
         # text = Coradoc::Generator.gen_adoc(@content)
-        content = Array(@content).map do |subitem|
-          next if subitem.is_a? Inline::HardLineBreak
+        content = Array(@content).flatten.compact
+        out = ""
+        prev_inline = :init
 
+        # Collapse meaningless <DIV>s
+        while content.map(&:class) == [Section] && content.first.safe_to_collapse?
+          content = Array(content.first.contents)
+        end
+
+        content.each do |subitem|
           subcontent = Coradoc::Generator.gen_adoc(subitem)
+
           # Only try to postprocess elements that are text,
           # otherwise we could strip markup.
-          if Coradoc.a_single?(subitem, Coradoc::Element::TextElement)
+          if subitem.is_a? Coradoc::Element::TextElement
             subcontent = Coradoc.strip_unicode(subcontent)
           end
-          subcontent
-        end.compact.join("\n+\n")
+
+          inline = inline?(subitem)
+
+          case inline
+          when true
+            if prev_inline == false
+              out += "\n+\n" + subcontent
+            else
+              out += subcontent
+            end
+          when false
+            case prev_inline
+            when :hardbreak
+              out += subcontent.strip
+            when :init
+              out += "{empty}\n+\n" + subcontent.strip
+            else
+              out += "\n+\n" + subcontent.strip
+            end
+          when :hardbreak
+            if %i[hardbreak init].include? prev_inline
+              # can't have two hard breaks in a row; can't start with a hard break
+            else
+              out += "\n+\n"
+            end
+          end
+
+          prev_inline = inline
+        end
+        out += "{empty}" if prev_inline == :hardbreak
+        out = "{empty}" if out.empty?
+
         # attach = Coradoc::Generator.gen_adoc(@attached)
         attach = @attached.map do |elem|
           "+\n" + Coradoc::Generator.gen_adoc(elem)
         end.join
         nest = Coradoc::Generator.gen_adoc(@nested)
-        out = " #{anchor}#{content}#{@line_break}"
+        out = " #{anchor}#{out}#{@line_break}"
         out + attach + nest
       end
     end
