81909795d8b36f9a9915a026af2d6ae766430d47
Jeffrey Lau <jeffrey.lau@ribose.com>
2025-03-19 15:17:12 +0800

    test: Add acceptance test for video tag conversion
    Related: #147

---
+++ spec/acceptance/conversion_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+RSpec.describe "Conversion" do
+  let(:output) { StringIO.new }
+
+  before do
+    Coradoc::Converter.call(
+      input,
+      output,
+      input_processor: input_format,
+      output_processor: output_format,
+    )
+  end
+
+  describe "from html to asciidoc" do
+    let(:input_format) { :html }
+    let(:output_format) { :adoc }
+
+    describe "video tags" do
+      let(:input) { StringIO.new("<video src='example.mp4' />") }
+      subject(:output_string) { output.string }
+
+      it { is_expected.to eq "video::example.mp4[]" }
+    end
+  end
+end
