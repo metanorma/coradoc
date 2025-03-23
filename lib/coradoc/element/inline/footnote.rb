527a45ee017dfe51c5e0c572e75da04dd6cb3ea9
Ronald Tse <ronald.tse@ribose.com>
2025-03-23 13:22:12 +0800

    fix: spec fixes

--- lib/coradoc/element/inline/footnote.rb
+++ lib/coradoc/element/inline/footnote.rb
@@ -2,16 +2,21 @@ module Coradoc
   module Element
     module Inline
       class Footnote < Base
-        attr_accessor :text
+        attr_accessor :text, :id
 
-        declare_children :text
+        declare_children :text, :id
 
-        def initialize(text, _options = {})
+        def initialize(text, id = nil)
           @text = text
+          @id = id
         end
 
         def to_adoc
-          "footnote:[#{@text}]"
+          if @id
+            "footnote:#{@id}[#{@text}]"
+          else
+            "footnote:[#{@text}]"
+          end
         end
       end
     end
