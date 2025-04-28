8892517b241ed9a3247594892929150312e2f485
Jeffrey Lau <jeffrey.lau@ribose.com>
2025-04-28 18:30:08 +0800

    fix(lutaml): Add shim for "#simplify_block_content"

--- lib/coradoc/model/base.rb
+++ lib/coradoc/model/base.rb
@@ -6,6 +6,17 @@ module Coradoc
   module Model
     class Base < Lutaml::Model::Serializable
       attribute :id, :string
+
+      # Generate a warning message whenever this method is called.
+      def simplify_block_content(content)
+        warn "[DEPRECATION] #simplify_block_content is called inside a Lutaml Model.  This is still a WIP."
+        # print part of the stack trace
+        caller_locations(1, 3).each do |location|
+          warn "  #{location.path}:#{location.lineno} in #{location.label}"
+        end
+
+        content
+      end
     end
   end
 end
