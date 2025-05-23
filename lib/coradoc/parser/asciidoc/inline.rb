9bf6b43421d17b3c4fc411f8399878ada240e805
Jeffrey Lau <jeffrey.lau@ribose.com>
2025-05-23 17:41:30 +0800

    fix: Make inline image with no attributes parseable

--- lib/coradoc/parser/asciidoc/inline.rb
+++ lib/coradoc/parser/asciidoc/inline.rb
@@ -117,7 +117,7 @@ module Coradoc
         def inline_image
           (str("image:").present? >> str("image:") >>
             match("[A-Za-z0-9_.\\-:/&?=+,%#~;]+").repeat(1).as(:path) >>
-            (str("[") >> match("[^\\]]").repeat(1).as(:text) >> str("]")).maybe
+            (str("[") >> match("[^\\]]").repeat.as(:text) >> str("]")).maybe
           ).as(:inline_image)
         end
 
