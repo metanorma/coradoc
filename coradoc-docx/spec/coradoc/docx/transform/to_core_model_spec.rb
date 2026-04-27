2852871269918a61ebdbaf5a5b1e422c99174806
Ronald Tse <ronald.tse@ribose.com>
2026-04-28 07:00:00 +0800

    test: add DOCX ToCoreModel specs for sub/superscript, highlight, nested headings, page breaks

--- coradoc-docx/spec/coradoc/docx/transform/to_core_model_spec.rb
+++ coradoc-docx/spec/coradoc/docx/transform/to_core_model_spec.rb
@@ -188,6 +188,7 @@ RSpec.describe Coradoc::Docx::Transform::ToCoreModel do
         inline = blocks.first.children.find { |c| c.is_a?(Coradoc::CoreModel::InlineElement) }
         expect(inline).not_to be_nil
         expect(inline.format_type).to eq('underline')
+        expect(inline.content).to eq('underlined')
       end
 
       it 'converts strikethrough run to InlineElement' do
@@ -200,6 +201,111 @@ RSpec.describe Coradoc::Docx::Transform::ToCoreModel do
         inline = blocks.first.children.find { |c| c.is_a?(Coradoc::CoreModel::InlineElement) }
         expect(inline).not_to be_nil
         expect(inline.format_type).to eq('strikethrough')
+        expect(inline.content).to eq('deleted')
+      end
+    end
+
+    context 'with subscript and superscript' do
+      it 'converts subscript run to InlineElement' do
+        doc = build_document
+        para = build_paragraph('H')
+        sub_run = Uniword::Wordprocessingml::Run.new
+        sub_run.text = Uniword::Wordprocessingml::Text.new(content: '2')
+        sub_run.properties = Uniword::Wordprocessingml::RunProperties.new
+        va = Uniword::Properties::VerticalAlign.new
+        va.value = 'subscript'
+        sub_run.properties.vertical_align = va
+        para.runs << sub_run
+        doc.body.paragraphs << para
+
+        core = transform_to_core(doc)
+
+        blocks = core.children.select { |c| c.is_a?(Coradoc::CoreModel::Block) }
+        inline = blocks.first.children.find { |c| c.is_a?(Coradoc::CoreModel::InlineElement) && c.format_type == 'subscript' }
+        expect(inline).not_to be_nil
+        expect(inline.content).to eq('2')
+      end
+
+      it 'converts superscript run to InlineElement' do
+        doc = build_document
+        para = build_paragraph('x')
+        sup_run = Uniword::Wordprocessingml::Run.new
+        sup_run.text = Uniword::Wordprocessingml::Text.new(content: '2')
+        sup_run.properties = Uniword::Wordprocessingml::RunProperties.new
+        va = Uniword::Properties::VerticalAlign.new
+        va.value = 'superscript'
+        sup_run.properties.vertical_align = va
+        para.runs << sup_run
+        doc.body.paragraphs << para
+
+        core = transform_to_core(doc)
+
+        blocks = core.children.select { |c| c.is_a?(Coradoc::CoreModel::Block) }
+        inline = blocks.first.children.find { |c| c.is_a?(Coradoc::CoreModel::InlineElement) && c.format_type == 'superscript' }
+        expect(inline).not_to be_nil
+        expect(inline.content).to eq('2')
+      end
+    end
+
+    context 'with highlight' do
+      it 'converts highlight run to InlineElement' do
+        doc = build_document
+        run = Uniword::Wordprocessingml::Run.new
+        run.text = Uniword::Wordprocessingml::Text.new(content: 'highlighted')
+        run.properties = Uniword::Wordprocessingml::RunProperties.new
+        run.properties.highlight = Uniword::Properties::Highlight.new
+        doc.body.paragraphs << build_paragraph(run)
+
+        core = transform_to_core(doc)
+
+        blocks = core.children.select { |c| c.is_a?(Coradoc::CoreModel::Block) }
+        inline = blocks.first.children.find { |c| c.is_a?(Coradoc::CoreModel::InlineElement) }
+        expect(inline).not_to be_nil
+        expect(inline.format_type).to eq('highlight')
+        expect(inline.content).to eq('highlighted')
+      end
+    end
+
+    context 'with nested headings' do
+      it 'creates separate sections from H2 and H3' do
+        doc = build_document
+        doc.body.paragraphs << build_heading('Title', level: 1)
+        doc.body.paragraphs << build_heading('Section 1', level: 2)
+        doc.body.paragraphs << build_paragraph('Content of section 1')
+        doc.body.paragraphs << build_heading('Sub-section 1.1', level: 3)
+        doc.body.paragraphs << build_paragraph('Sub-section content')
+
+        core = transform_to_core(doc)
+
+        expect(core.title).to eq('Title')
+        sections = core.children.select { |c| c.is_a?(Coradoc::CoreModel::StructuralElement) }
+        expect(sections.length).to eq(2)
+        expect(sections[0].title).to eq('Section 1')
+        expect(sections[0].level).to eq(2)
+        expect(sections[1].title).to eq('Sub-section 1.1')
+        expect(sections[1].level).to eq(3)
+      end
+    end
+
+    context 'with page breaks' do
+      it 'converts page break run to Block with page_break element_type' do
+        doc = build_document
+        doc.body.paragraphs << build_paragraph('Page 1')
+
+        break_para = Uniword::Wordprocessingml::Paragraph.new
+        break_run = Uniword::Wordprocessingml::Run.new
+        break_run.break = Uniword::Wordprocessingml::Break.new
+        break_run.break.type = 'page'
+        break_para.runs << break_run
+        doc.body.paragraphs << break_para
+
+        core = transform_to_core(doc)
+
+        # Page break is nested inside a paragraph Block's children
+        all_blocks = core.children.select { |c| c.is_a?(Coradoc::CoreModel::Block) }
+        nested = all_blocks.flat_map(&:children).select { |c| c.is_a?(Coradoc::CoreModel::Block) }
+        page_break = nested.find { |b| b.element_type == 'page_break' }
+        expect(page_break).not_to be_nil
       end
     end
   end
