26b92e7f2a0194d0dc2229a9a438f1fba8d5f612
Ronald Tse <ronald.tse@ribose.com>
2026-05-01 19:01:02 +0800

    refactor: extract ProcessorRegistry to DRY Input and Output modules
    Input and Output duplicated identical registration logic. Extract
    shared ProcessorRegistry module with define/get/registered?/for_file/
    process. Both Input and Output now extend ProcessorRegistry.

--- lib/coradoc/output.rb
+++ lib/coradoc/output.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'processor_registry'
+
 module Coradoc
   # Output module for document serialization
   #
@@ -14,64 +16,7 @@ module Coradoc
   #   html = processor.processor_execute(document, {})
   #
   module Output
-    class << self
-      # Get all registered processors
-      # @return [Hash<Symbol, Module>] Registered processors
-      def processors
-        @processors ||= {}
-      end
-
-      # Register an output processor
-      # @param processor [Module] Processor module to register
-      # @return [void]
-      def define(processor)
-        return unless processor.respond_to?(:processor_id)
-
-        processors[processor.processor_id] = processor
-      end
-
-      # Get a processor by ID
-      # @param id [Symbol] Processor ID
-      # @return [Module, nil] Processor module or nil
-      def get(id)
-        processors[id.to_sym]
-      end
-      alias [] get
-
-      # Check if a processor is registered
-      # @param id [Symbol] Processor ID
-      # @return [Boolean]
-      def registered?(id)
-        processors.key?(id.to_sym)
-      end
-
-      # Find processor that matches a filename
-      # @param filename [String] Filename to match
-      # @return [Module, nil] Matching processor or nil
-      def for_file(filename)
-        processors.values.find do |processor|
-          processor.respond_to?(:processor_match?) &&
-            processor.processor_match?(filename)
-        end
-      end
-
-      # Process output with the appropriate processor
-      # @param document [Object] Document to serialize
-      # @param options [Hash] Processing options
-      # @option options [Symbol] :format Output format (optional, auto-detected if not provided)
-      # @option options [String] :filename Filename for format detection
-      # @return [Object] Serialized output
-      def process(document, options = {})
-        processor = if options[:format]
-                      get(options[:format])
-                    elsif options[:filename]
-                      for_file(options[:filename])
-                    end
-
-        raise ArgumentError, "No output processor found for: #{options}" unless processor
-
-        processor.processor_execute(document, options)
-      end
-    end
+    extend ProcessorRegistry
+    self.error_label = "output processor"
   end
 end
