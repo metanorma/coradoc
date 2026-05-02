4dad7ac9d85c9efa9b819f67f4d0b75c966f3f98
Ronald Tse <ronald.tse@ribose.com>
2026-05-02 13:27:16 +0800

    perf: eliminate Logger per-call instance allocation
    Logger.error/info/warn/success were creating a throwaway instance per
    log call via new.call. Converted to class methods with private_class_method.

--- lib/coradoc/logger.rb
+++ lib/coradoc/logger.rb
@@ -15,49 +15,40 @@ module Coradoc
     }.freeze
 
     def self.error(message)
-      new.call(message, :error)
+      log(message, :error)
     end
 
     def self.info(message)
-      new.call(message, :info)
+      log(message, :info)
     end
 
     def self.success(message)
-      new.call(message, :success)
+      log(message, :success)
     end
 
     def self.warn(message)
-      new.call(message, :warn)
+      log(message, :warn)
     end
 
-    def call(message, type)
+    def self.log(message, type)
       Warning.warn format_message(message, type)
     end
 
-    private
-
-    def color(type)
-      if COLORS.key?(type)
-        COLORS[type]
-      else
-        raise ArgumentError,
-              "Unknown log type: #{type}. Available types: #{COLORS.keys.join(', ')}"
-      end
-    end
-
-    def colorize(message, type)
-      io = type == :warn ? $stderr : $stdout
-      return message unless io.tty?
-
-      "#{color(type)}#{message}#{COLORS[:reset]}"
-    end
-
-    def format_message(message, type)
+    def self.format_message(message, type)
       colorize(
         "\n[#{BADGE}] #{COLORS[:bold]}#{type.upcase}#{COLORS[:unbold]}" \
         ": #{message}\n",
         type
       )
     end
+
+    def self.colorize(message, type)
+      io = type == :warn ? $stderr : $stdout
+      return message unless io.tty?
+
+      "#{COLORS[type]}#{message}#{COLORS[:reset]}"
+    end
+
+    private_class_method :log, :format_message, :colorize
   end
 end
